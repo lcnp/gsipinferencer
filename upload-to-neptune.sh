56fd59e6eb5a96d0fd9c7f0f2a40dfc497d1472b
jabdali <100700035+jabdali@users.noreply.github.com>
2023-09-06 23:12:01 -0400

    Update upload-to-neptune.sh curl in same line

--- upload-to-neptune.sh
+++ upload-to-neptune.sh
@@ -7,9 +7,7 @@ echo "folder name:" $4
 echo "Running curl command.."
 echo "* * * * * * * * * * * * * * * * * * * * * * * * * *" 
 echo The URL of the curl command is https://$1:8182/loader
-curl -v  \
-    -H 'Content-Type: application/json' \
-     https://$1:8182/loader -d '
+curl -v -H 'Content-Type: application/json' https://$1:8182/loader -d '
     {
       "source" : "s3://'$3'/'$4'",
       "format" : "turtle",
